e2c10e2ca02989d6f0880c7b844b1af995ad5107
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-11-02 18:37:18 +0530

    Calculating Wages For A Month.

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -5,6 +5,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
+let empHours = 0;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -17,8 +19,10 @@ function getWorkingHours(empCheck) {
     } 
 }
 
-let empHours = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHours = getWorkingHours(empCheck);
+for (let day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHours += getWorkingHours(empCheck);
+}
+
 let empWage = empHours * WAGE_PER_HOUR;
-console.log(`Emp Wage: ${empWage}`);
+console.log(`Total Hours: ${empHours}, Emp Wage: ${empWage}`);
